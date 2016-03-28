91f2a6b1c4a6c547131df8cb1801c9612026651e
Alex Z <derk153@gmail.com>
2016-03-28 11:55:32 -0600

    add bower

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 angular.module('flapperNews', [])
   .controller('MainCtrl', [
-    '$scope',
-    function($scope) {
+    '$scope', 'posts',
+    function($scope, posts) {
       $scope.test = 'Hello world!';
 
-      $scope.posts = [{
-        title: 'post 1',
-        upvotes: 5
-      }, {
-        title: 'post 2',
-        upvotes: 2
-      }, {
-        title: 'post 3',
-        upvotes: 15
-      }, {
-        title: 'post 4',
-        upvotes: 9
-      }, {
-        title: 'post 5',
-        upvotes: 4
-      }];
+      $scope.posts = posts.posts;
 
       $scope.addPost = function() {
-        if(!$scope.title || $scope.title === '') { return; }
+        if (!$scope.title || $scope.title === '') {
+          return;
+        }
         $scope.posts.push({
           title: $scope.title,
           link: $scope.link,
@@ -35,6 +22,11 @@ angular.module('flapperNews', [])
       $scope.incrementUpvotes = function(post) {
         post.upvotes += 1;
       };
-
     }
-  ]);
+  ])
+  .factory('posts', [function() {
+    var o = {
+      posts: []
+    };
+    return o;
+  }]);
