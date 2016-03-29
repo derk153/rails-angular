bfb2b9f56b773f6e170e0d6aea20eb35aa0f1938
Alex Z <derk153@gmail.com>
2016-03-29 15:08:25 -0600

    add devise

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  link       :string
+#  upvotes    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ActiveRecord::Base
   has_many :comments
 
